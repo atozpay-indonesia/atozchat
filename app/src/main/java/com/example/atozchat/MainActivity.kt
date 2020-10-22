25c51f85f0b73464ed261a4d8f83b258946e1afa
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2020-10-22 15:08:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/atozchat/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.atozchat
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.atozchatlibrary.PersonalChatRoomActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ atozchatlibrary/src/main/java/com/example/atozchatlibrary/PersonalChatRoomActivity.kt
@@ -0,0 +1,12 @@
+package com.example.atozchatlibrary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.atozchatlibrary.R.layout.activity_chat_room_personal
+
+class PersonalChatRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_chat_room_personal)
+    }
+}
