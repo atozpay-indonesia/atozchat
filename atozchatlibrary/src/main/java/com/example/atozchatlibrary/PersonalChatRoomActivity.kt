394c937fb2ed3f36a49fcea7ca67fa128c134738
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2020-10-23 16:11:54 +0700

    add Firestore library to project

--- atozchatlibrary/src/main/java/com/example/atozchatlibrary/PersonalChatRoomActivity.kt
+++ atozchatlibrary/src/main/java/com/example/atozchatlibrary/PersonalChatRoomActivity.kt
@@ -3,8 +3,11 @@ package com.example.atozchatlibrary
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.atozchatlibrary.R.layout.activity_chat_room_personal
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PersonalChatRoomActivity : AppCompatActivity() {
+    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_chat_room_personal)
