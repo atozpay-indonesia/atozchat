8c459670c34e7eb5275aa97b271ff8d4cc903df8
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2020-10-23 17:27:36 +0700

    update personal chat room page

--- atozchatlibrary/src/main/java/com/example/atozchatlibrary/PersonalChatRoomActivity.kt
+++ atozchatlibrary/src/main/java/com/example/atozchatlibrary/PersonalChatRoomActivity.kt
@@ -3,10 +3,15 @@ package com.example.atozchatlibrary
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.atozchatlibrary.R.layout.activity_chat_room_personal
+import com.example.atozchatlibrary.model.Chat
 import com.google.firebase.firestore.FirebaseFirestore
+import java.util.ArrayList
 
 class PersonalChatRoomActivity : AppCompatActivity() {
     var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    private val chatList: MutableList<Chat> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
