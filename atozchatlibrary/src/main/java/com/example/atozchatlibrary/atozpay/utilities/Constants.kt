f6b6f1eb19fba4604d079b47a25ab6809d5b03a8
Muhammad Abdi Alidrus <muhammadabdi25@gmail.com>
2020-11-25 11:58:31 +0700

    create constants class

---
+++ atozchatlibrary/src/main/java/com/example/atozchatlibrary/atozpay/utilities/Constants.kt
@@ -0,0 +1,33 @@
+package com.example.atozchatlibrary.atozpay.utilities
+
+class Constants {
+
+    companion object {
+        const val PERSONAL_CHAT_TYPE_OUTGOING = 1
+        const val PERSONAL_CHAT_TYPE_INCOMING = 2
+        const val CHAT_SNIPPET_LENGTH = 50
+
+        const val COLLECTION_ROOT = "messaging"
+
+        const val ROOM_DOC_FIELD_NAME_CURRENT_CHATS_ID = "current_chats_id"
+        const val ROOM_DOC_FIELD_NAME_FIRST_USER_ID = "first_user_id"
+        const val ROOM_DOC_FIELD_NAME_FIRST_USER_NAME = "first_user_name"
+        const val ROOM_DOC_FIELD_NAME_SECOND_USER_ID = "second_user_id"
+        const val ROOM_DOC_FIELD_NAME_SECOND_USER_NAME = "second_user_name"
+        const val ROOM_DOC_FIELD_NAME_IS_CUSTOMER_ONLINE = "is_customer_online"
+        const val ROOM_DOC_FIELD_NAME_IS_NEW_CHAT_AVAILABLE = "is_new_chat_available"
+        const val ROOM_DOC_FIELD_NAME_LAST_CHAT = "last_chat"
+        const val ROOM_DOC_FIELD_NAME_LAST_UPDATE = "last_update"
+        const val ROOM_DOC_FIELD_NAME_SESSION_STATUS = "session_status"
+        const val ROOM_DOC_FIELD_NAME_SESSION_START_AT = "session_start_at"
+        const val ROOM_DOC_FIELD_NAME_SESSION_END_AT = "session_end_at"
+
+        const val CHAT_DOC_FIELD_NAME_CHAT_BODY = "chat_body"
+        const val CHAT_DOC_FIELD_NAME_RECIPIENT_ID = "recipient_id"
+        const val CHAT_DOC_FIELD_NAME_RECIPIENT_NAME = "recipient_name"
+        const val CHAT_DOC_FIELD_NAME_SENDER_ID = "sender_id"
+        const val CHAT_DOC_FIELD_NAME_SENDER_NAME = "sender_name"
+        const val CHAT_DOC_FIELD_NAME_TIME_SENT = "time_sent"
+    }
+
+}
